8e2a5b06bdfb83475f639c03e5442fd7f0d1b326
OSCAR BARRERA <oskarbarrera1@yahoo.es>
2022-11-14 19:51:16 -0500

    46, 53 , 59 commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -43,19 +43,20 @@ function suma(x, y) {
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  
+  return y - x
+
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  
+  return x * y
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  
+  return x / y 
 }
 
 function sonIguales(x, y) {
